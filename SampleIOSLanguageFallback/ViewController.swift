f7d5c562e7e3ff6a953da11cc34ec0d7ac77ab6a
moko <yukie.hr@gmail.com>
2016-09-28 22:40:56 +0900

    Display each languages params

--- SampleIOSLanguageFallback/ViewController.swift
+++ SampleIOSLanguageFallback/ViewController.swift
@@ -10,16 +10,50 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var deviceLanguage: UILabel!
+    @IBOutlet weak var deviceLanguages: UILabel!
+    @IBOutlet weak var appSelectedLanguage: UILabel!
+    @IBOutlet weak var appSelectedLanguages: UILabel!
+    @IBOutlet weak var appLanguages: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let deviceLanguagesFromLocale = NSLocale.preferredLanguages
+        let appSelectedLanguagesFromBundle = Bundle.main.preferredLocalizations
+        let appLanguagesFromBundle = Bundle.main.localizations
+        
+        deviceLanguage.text = deviceLanguagesFromLocale.first
+        
+        var languagesString = ""
+        let _ = deviceLanguagesFromLocale.enumerated().map { offset, string in
+            languagesString += string
+            if offset != deviceLanguagesFromLocale.count - 1 {
+                languagesString += "\n"
+            }
+        }
+        deviceLanguages.text = languagesString
+        
+        appSelectedLanguage.text = appSelectedLanguagesFromBundle.first
+        
+        var appSelectedLanguagesString = ""
+        let _ = appSelectedLanguagesFromBundle.enumerated().map { offset, string in
+            appSelectedLanguagesString += string
+            if offset != appSelectedLanguagesFromBundle.count - 1 {
+                appSelectedLanguagesString += "\n"
+            }
+        }
+        appSelectedLanguages.text = appSelectedLanguagesString
+        
+        var appLanguagesString = ""
+        appLanguagesFromBundle.forEach { (string) in
+            appLanguagesString += "\(string)\n"
+        }
+        appLanguages.text = appLanguagesString
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
-
-
 }
 
