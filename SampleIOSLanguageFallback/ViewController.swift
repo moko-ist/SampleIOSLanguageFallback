228bb16c534548eb85a27e6783e6ab80293b4f1a
moko <yukie.hr@gmail.com>
2016-09-28 14:37:16 +0900

    Initial commit

---
+++ SampleIOSLanguageFallback/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SampleIOSLanguageFallback
+//
+//  Created by Yukie Hori on 2016/09/28.
+//  Copyright © 2016年 moko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
